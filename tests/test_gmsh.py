7d6d2075e25477d2f0185243c142d18aa2d58cf8
Tetsuo Koyama <tkoyama010@gmail.com>
2022-07-27 17:33:39 +0900

    Update test_gmsh.py

--- tests/test_gmsh.py
+++ tests/test_gmsh.py
@@ -5,16 +5,10 @@ import pyvista
 def test_tutorial1():
     # Gmsh Python tutorial 1
     # https://gitlab.onelab.info/gmsh/gmsh/blob/gmsh_4_10_5/tutorials/python/t1.py
-
     vertices = np.array([[0, 0, 0], [0.1, 0, 0], [0.1, 0.3, 0], [0.3, 0, 0]])
     faces = np.hstack([[4, 0, 1, 2, 3]])
     surf = pyvista.PolyData(vertices, faces)
-
     # pvgmsh does not support PhysicalGroup; group configuration can be easily done with PyVista.
-
     mesh = pvgmsh.generate_mesh(surf)
     assert mesh.n_pts > surf.n_pts
     assert mesh.n_cells > surf.n_cells
-
-    mesh.save("t1.msh")
-    mesh.plot()
