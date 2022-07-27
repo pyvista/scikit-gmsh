69e747f4442a85e2c696af042f50147b9d5f03ba
Tetsuo Koyama <tkoyama010@gmail.com>
2022-07-27 16:45:35 +0900

    Update test_gmsh.py

--- tests/test_gmsh.py
+++ tests/test_gmsh.py
@@ -6,18 +6,13 @@ def test_tutorial1():
     # https://gitlab.onelab.info/gmsh/gmsh/blob/gmsh_4_10_5/tutorials/python/t1.py
 
     vertices = np.array([[0, 0, 0], [0.1, 0, 0], [0.1, 0.3, 0], [0.3, 0, 0]])
-
     faces = np.hstack([[4, 0, 1, 2, 3]])
-
     surf = pvgmsh.PolyData(vertices, faces)
 
-    surf["points group number"] = np.array([1, 1, 1])
-    surf["surface group number"] = np.array([2])
-    surf["lc"] = np.array([1e-2, 1e-2, 1e-2])
+    surf.set_characteristic_length([1e-2, 1e-2, 1e-2, 1e-2])
 
-    surf.add_physical_group(["points group number", "surface group number"])
+    # pvgmsh does not support PhysicalGroup; group configuration can be easily done with PyVista.
+
     mesh = surf.generate_mesh()
-
     mesh.save("t1.msh")
-
     mesh.plot()
