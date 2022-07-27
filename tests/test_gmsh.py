78705f40b9826a91f4a911beb4f4f20391acd85b
Tetsuo Koyama <tkoyama010@gmail.com>
2022-07-27 16:50:01 +0900

    Update test_gmsh.py

--- tests/test_gmsh.py
+++ tests/test_gmsh.py
@@ -9,8 +9,6 @@ def test_tutorial1():
     faces = np.hstack([[4, 0, 1, 2, 3]])
     surf = pvgmsh.PolyData(vertices, faces)
 
-    surf.set_characteristic_length([1e-2, 1e-2, 1e-2, 1e-2])
-
     # pvgmsh does not support PhysicalGroup; group configuration can be easily done with PyVista.
 
     mesh = surf.generate_mesh()
