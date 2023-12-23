44230effa5ef426c90b3d491719e35a1eff06cf4
Tetsuo Koyama <tkoyama010@gmail.com>
2023-12-23 14:18:36 +0900

    Update

--- pvgmsh/__init__.py
+++ pvgmsh/__init__.py
@@ -8,10 +8,10 @@ def generate_mesh(surf, lc = 1e-2):
     for i, point in enumerate(surf.points):
         gmsh.model.geo.addPoint(point[0], point[1], point[2], lc, i + 1)
     gmsh.model.geo.addLine(1, 2, 1)
-    gmsh.model.geo.addLine(3, 2, 2)
+    gmsh.model.geo.addLine(2, 3, 2)
     gmsh.model.geo.addLine(3, 4, 3)
     gmsh.model.geo.addLine(4, 1, 4)
-    gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
     gmsh.model.geo.addPlaneSurface([1], 1)
     gmsh.model.geo.synchronize()
     gmsh.model.mesh.generate(2)
