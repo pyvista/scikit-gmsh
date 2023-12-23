392f7d1aec373ff220613aeb7f659b62ada559ef
Tetsuo Koyama <tkoyama010@gmail.com>
2023-12-23 14:16:08 +0900

    Update point operation

--- pvgmsh/__init__.py
+++ pvgmsh/__init__.py
@@ -3,17 +3,14 @@ import pyvista as pv
 from pvgmsh._version import __version__  # noqa: F401
 
 
-def generate_mesh(surf):
+def generate_mesh(surf, lc = 1e-2):
     gmsh.initialize()
-    lc = 1e-2
-    gmsh.model.geo.addPoint(0, 0, 0, lc, 1)
-    gmsh.model.geo.addPoint(0.1, 0, 0, lc, 2)
-    gmsh.model.geo.addPoint(0.1, 0.3, 0, lc, 3)
-    p4 = gmsh.model.geo.addPoint(0, 0.3, 0, lc)
+    for i, point in enumerate(surf.points):
+        gmsh.model.geo.addPoint(point[0], point[1], point[2], lc, i + 1)
     gmsh.model.geo.addLine(1, 2, 1)
     gmsh.model.geo.addLine(3, 2, 2)
-    gmsh.model.geo.addLine(3, p4, 3)
-    gmsh.model.geo.addLine(4, 1, p4)
+    gmsh.model.geo.addLine(3, 4, 3)
+    gmsh.model.geo.addLine(4, 1, 4)
     gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
     gmsh.model.geo.addPlaneSurface([1], 1)
     gmsh.model.geo.synchronize()
