6f90d498aa8c7e4e7dfe736e8e01609b5bf831d3
Tetsuo Koyama <tkoyama010@gmail.com>
2023-12-23 17:54:36 +0900

    Move example to docstring for doctest

--- pvgmsh/__init__.py
+++ pvgmsh/__init__.py
@@ -4,13 +4,64 @@ import tempfile
 from pvgmsh._version import __version__  # noqa: F401
 
 
-def generate_mesh(surf, lc=1e-2, dimension=2):
+def delaunay_2d(poly_data, size=1e-2):
+    """
+    Parameters
+    ----------
+    poly_data : pv.PolyData
+        Poly data to generate delaunay 2d mesh.
+
+    size : float, optional
+        Target mesh size close to the point.
+        Defalut 1e-2.
+
+    Returns
+    -------
+    pyvista.UnstructuredGrid
+        Mesh from the 2D delaunay generation.
+
+    Examples
+    --------
+    This example is inspired by `Gmsh Python tutorial 1 <https://gitlab.onelab.info/gmsh/gmsh/blob/gmsh_4_10_5/tutorials/python/t1.py>`_.
+
+    >>> import pvgmsh
+    >>> import pyvista as pv
+    >>> import numpy as np
+
+    Define poly_data using PyVista.
+
+    >>> vertices = np.array([[0, 0, 0], [0.1, 0, 0], [0.1, 0.3, 0], [0, 0.3, 0]])
+    >>> faces = np.hstack([[4, 0, 1, 2, 3]])
+    >>> poly_data = pv.PolyData(vertices, faces)
+    >>> poly_data
+    PolyData (...)
+      N Cells:    1
+      N Points:   4
+      N Strips:   0
+      X Bounds:   0.000e+00, 1.000e-01
+      Y Bounds:   0.000e+00, 3.000e-01
+      Z Bounds:   0.000e+00, 0.000e+00
+      N Arrays:   0
+
+    Generate mesh using gmsh.
+
+    >>> mesh = pvgmsh.delaunay_2d(poly_data)
+    <BLANKLINE>
+    >>> mesh
+    UnstructuredGrid (...)
+      N Cells:    816
+      N Points:   407
+      X Bounds:   0.000e+00, 1.000e-01
+      Y Bounds:   0.000e+00, 3.000e-01
+      Z Bounds:   0.000e+00, 0.000e+00
+      N Arrays:   2
+    """
     meshes = []
-    for cell in surf.cell:
+    for cell in poly_data.cell:
         if cell.type == pv.CellType.QUAD:
             gmsh.initialize()
             for i, point in enumerate(cell.points):
-                gmsh.model.geo.addPoint(point[0], point[1], point[2], lc, i + 1)
+                gmsh.model.geo.addPoint(point[0], point[1], point[2], size, i + 1)
             gmsh.model.geo.addLine(1, 2, 1)
             gmsh.model.geo.addLine(2, 3, 2)
             gmsh.model.geo.addLine(3, 4, 3)
@@ -18,13 +69,13 @@ def generate_mesh(surf, lc=1e-2, dimension=2):
             gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
             gmsh.model.geo.addPlaneSurface([1], 1)
             gmsh.model.geo.synchronize()
-            gmsh.model.mesh.generate(dimension)
+            gmsh.model.mesh.generate(2)
             with tempfile.NamedTemporaryFile(
                 mode="w+", encoding="utf-8", newline="\n", suffix=".msh"
             ) as fp:
                 gmsh.write(fp.name)
                 mesh = pv.read(fp.name)
-                if surf.number_of_cells == 1:
+                if poly_data.number_of_cells == 1:
                     gmsh.clear()
                     gmsh.finalize()
                     return mesh
