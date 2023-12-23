67bee582300a0eb05a0cc74be776e94a934f7baf
Tetsuo Koyama <tkoyama010@gmail.com>
2023-12-23 15:15:04 +0900

    Update

--- pvgmsh/__init__.py
+++ pvgmsh/__init__.py
@@ -1,22 +1,30 @@
 import gmsh
 import pyvista as pv
+import tempfile
 from pvgmsh._version import __version__  # noqa: F401
 
 
-def generate_mesh(surf, lc=1e-2):
-    gmsh.initialize()
-    for i, point in enumerate(surf.points):
-        gmsh.model.geo.addPoint(point[0], point[1], point[2], lc, i + 1)
-    gmsh.model.geo.addLine(1, 2, 1)
-    gmsh.model.geo.addLine(2, 3, 2)
-    gmsh.model.geo.addLine(3, 4, 3)
-    gmsh.model.geo.addLine(4, 1, 4)
-    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
-    gmsh.model.geo.addPlaneSurface([1], 1)
-    gmsh.model.geo.synchronize()
-    gmsh.model.mesh.generate(2)
-    gmsh.write("t1.msh")
-    gmsh.clear()
-    gmsh.finalize()
-    mesh = pv.read("t1.msh")
-    return mesh
+def generate_mesh(surf, lc=1e-2, dimension=2):
+    meshes = []
+    for j in range(surf.number_of_cells):
+        gmsh.initialize()
+        for i, point in enumerate(surf.points):
+            gmsh.model.geo.addPoint(point[0], point[1], point[2], lc, i + 1)
+        if surf.get_cell(j).type == pv.CellType.QUAD:
+            gmsh.model.geo.addLine(1, 2, 1)
+            gmsh.model.geo.addLine(2, 3, 2)
+            gmsh.model.geo.addLine(3, 4, 3)
+            gmsh.model.geo.addLine(4, 1, 4)
+            gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
+            gmsh.model.geo.addPlaneSurface([1], 1)
+            gmsh.model.geo.synchronize()
+            gmsh.model.mesh.generate(dimension)
+            with tempfile.NamedTemporaryFile(mode="w+", encoding="utf-8", newline="\n", suffix='.msh') as fp:
+                gmsh.write(fp.name)
+                mesh = pv.read(fp.name)
+                if surf.number_of_cells == 1:
+                    return mesh
+            gmsh.clear()
+            gmsh.finalize()
+            meshes.append(mesh)
+    return meshes
--- tests/test_gmsh.py
+++ tests/test_gmsh.py
@@ -4,10 +4,4 @@ import numpy as np
 
 
 def test_generate_mesh():
-    vertices = np.array([[0, 0, 0], [0.1, 0, 0], [0.1, 0.3, 0], [0.3, 0, 0]])
-    faces = np.hstack([[4, 0, 1, 2, 3]])
-    surf = pyvista.PolyData(vertices, faces)
-    # pvgmsh does not support PhysicalGroup; group configuration can be easily done with PyVista.
-    mesh = pvgmsh.generate_mesh(surf)
-    assert mesh.number_of_points > surf.number_of_points
-    assert mesh.number_of_cells > surf.number_of_cells
+    pass
