f7054bdfe610689d28f5674f91119d4c6c827061
Tetsuo Koyama <tkoyama010@gmail.com>
2023-12-29 14:41:55 +0900

    Update __init__.py

--- src/pvgmsh/__init__.py
+++ src/pvgmsh/__init__.py
@@ -47,7 +47,6 @@ def frontal_delaunay_2d(
 
     >>> import pyvista as pv
     >>> import pvgmsh as pm
-    >>> from pathlib import Path
 
     >>> edge_source = pv.Polygon(n_sides=4, radius=8, fill=False)
     >>> edge_source = edge_source.rotate_z(45, inplace=False)
