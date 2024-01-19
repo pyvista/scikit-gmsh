87d78489554e601256d15ab8e7ed3ea353f106da
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-19 18:02:45 +0900

    Create test_pvgmsh.py

---
+++ tests/test_pvgmsh.py
@@ -0,0 +1,15 @@
+"""PvGmsh package for 3D mesh generation test."""
+import pyvista as pv
+import pvgmsh as pm
+
+
+def test_frontal_delaunay_2d():
+    edge_source = pv.Polygon(n_sides=4, radius=8, fill=False)
+    mesh = pm.frontal_delaunay_2d(edge_source, target_size=2.0)
+    assert np.allclose(mesh.bounds, edge_source.bounds)
+
+
+def test_delaunay_3d():
+    edge_source = pv.Cube()
+    mesh = pm.delaunay_3d(edge_source, target_size=0.5)
+    assert np.allclose(mesh.bounds, edge_source.bounds)
