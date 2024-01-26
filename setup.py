88783551f7853043bd5875256aa53f618909bb50
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-26 14:44:08 +0900

    Add setup.py to fix the twine error uploading PyPI (#134)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""pvgmsh setuptools packaging."""
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name="pvgmsh", long_description=long_description, long_description_content_type="text/markdown")
