49ca45a1c41f02079215f99060c11ef6c71f34a9
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-26 10:43:05 +0900

    Update test_pvgmsh.py

--- tests/test_pvgmsh.py
+++ tests/test_pvgmsh.py
@@ -9,15 +9,15 @@ def test_frontal_delaunay_2d() -> None:
     """Frontal-Delaunay 2D mesh algorithm test code."""
     edge_source = pv.Polygon(n_sides=4, radius=8, fill=False)
     mesh = pm.frontal_delaunay_2d(edge_source, target_size=2.0)
-    assert np.allclose(mesh.bounds, edge_source.bounds)
     assert mesh.number_of_points > edge_source.number_of_points
     assert mesh.number_of_cells > edge_source.number_of_cells
+    assert np.allclose(mesh.volume, edge_source.volume)
 
 
 def test_delaunay_3d() -> None:
     """Delaunay 3D mesh algorithm test code."""
     edge_source = pv.Cube()
     mesh = pm.delaunay_3d(edge_source, target_size=0.5)
-    assert np.allclose(mesh.bounds, edge_source.bounds)
     assert mesh.number_of_points > edge_source.number_of_points
     assert mesh.number_of_cells > edge_source.number_of_cells
+    assert np.allclose(mesh.volume, edge_source.volume)
