b5c62ea7e2611e2eeb037e513ef155620eb114b0
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-27 00:05:00 +0900

    Support list in `target_sizes` to set each point (#138)

--- src/pvgmsh/__init__.py
+++ src/pvgmsh/__init__.py
@@ -3,12 +3,16 @@
 from __future__ import annotations
 
 import datetime
+from typing import TYPE_CHECKING
 
 import gmsh
 import numpy as np
 import pyvista as pv
 import scooby
 from pygmsh.helpers import extract_to_meshio
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
 
 FRONTAL_DELAUNAY_2D = 6
 DELAUNAY_3D = 1
@@ -24,7 +28,8 @@ __version__ = ".".join(map(str, version_info))
 
 def frontal_delaunay_2d(
     edge_source: pv.PolyData,
-    target_size: float | None,
+    *,
+    target_sizes: float | Sequence[float] | None = None,
 ) -> pv.PolyData | None:
     """
     Frontal-Delaunay 2D mesh algorithm.
@@ -39,7 +44,7 @@ def frontal_delaunay_2d(
         (i.e. point ids are identical in the input and
         source).
 
-    target_size : float, optional
+    target_sizes : float | Sequence[float], optional
         Target mesh size close to the points.
         Default max size of edge_source in each direction.
 
@@ -57,7 +62,7 @@ def frontal_delaunay_2d(
     >>> import pvgmsh as pm
 
     >>> edge_source = pv.Polygon(n_sides=4, radius=8, fill=False)
-    >>> mesh = pm.frontal_delaunay_2d(edge_source, target_size=2.0)
+    >>> mesh = pm.frontal_delaunay_2d(edge_source, target_sizes=2.0)
 
     >>> plotter = pv.Plotter(off_screen=True)
     >>> _ = plotter.add_mesh(mesh, show_edges=True, line_width=4, color="white", lighting=False, edge_color=[153, 153, 153])
@@ -73,10 +78,19 @@ def frontal_delaunay_2d(
     gmsh.initialize()
     gmsh.option.set_number("Mesh.Algorithm", FRONTAL_DELAUNAY_2D)
 
-    if target_size is None:
-        target_size = np.max(np.abs(bounds[1] - bounds[0]), np.abs(bounds[3] - bounds[2]), np.abs(bounds[5] - bounds[4]))
+    if target_sizes is None:
+        target_sizes = np.max(
+            [
+                np.abs(bounds[1] - bounds[0]),
+                np.abs(bounds[3] - bounds[2]),
+                np.abs(bounds[5] - bounds[4]),
+            ]
+        )
 
-    for i, point in enumerate(points):
+    if isinstance(target_sizes, float):
+        target_sizes = [target_sizes] * edge_source.number_of_points
+
+    for i, (target_size, point) in enumerate(zip(target_sizes, points)):
         id_ = i + 1
         gmsh.model.geo.add_point(point[0], point[1], point[2], target_size, id_)
 
@@ -100,7 +114,8 @@ def frontal_delaunay_2d(
 
 def delaunay_3d(
     edge_source: pv.PolyData,
-    target_size: float | None,
+    *,
+    target_sizes: float | Sequence[float] | None = None,
 ) -> pv.UnstructuredGrid | None:
     """
     Delaunay 3D mesh algorithm.
@@ -115,8 +130,9 @@ def delaunay_3d(
         (i.e. point ids are identical in the input and
         source).
 
-    target_size : float
+    target_sizes : float | Sequence[float], optional
         Target mesh size close to the points.
+        Default max size of edge_source in each direction.
 
     Returns
     -------
@@ -129,7 +145,7 @@ def delaunay_3d(
     >>> import pvgmsh as pm
 
     >>> edge_source = pv.Cube()
-    >>> mesh = pm.delaunay_3d(edge_source, target_size=0.4)
+    >>> mesh = pm.delaunay_3d(edge_source, target_sizes=0.4)
 
     >>> plotter = pv.Plotter(off_screen=True)
     >>> _ = plotter.add_mesh(mesh, show_edges=True, line_width=4, color="white", lighting=False, edge_color=[153, 153, 153])
@@ -150,11 +166,24 @@ def delaunay_3d(
     """
     points = edge_source.points
     faces = edge_source.regular_faces
+    bounds = edge_source.bounds
 
     gmsh.initialize()
     gmsh.option.set_number("Mesh.Algorithm3D", DELAUNAY_3D)
 
-    for i, point in enumerate(points):
+    if target_sizes is None:
+        target_sizes = np.max(
+            [
+                np.abs(bounds[1] - bounds[0]),
+                np.abs(bounds[3] - bounds[2]),
+                np.abs(bounds[5] - bounds[4]),
+            ]
+        )
+
+    if isinstance(target_sizes, float):
+        target_sizes = [target_sizes] * edge_source.number_of_points
+
+    for i, (point, target_size) in enumerate(zip(points, target_sizes)):
         id_ = i + 1
         gmsh.model.geo.add_point(point[0], point[1], point[2], target_size, id_)
 
