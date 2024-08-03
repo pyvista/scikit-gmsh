545e1055e6e2b87529ff50fe228af78169cdf968
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-03 21:01:01 +0900

    Update

--- cylinder.py
+++ cylinder.py
@@ -6,7 +6,7 @@ import pyvista as pv
 
 import skgmsh as sg
 
-edge_source = pv.Cylinder(resolution=4)
+edge_source = pv.Cylinder(resolution=16)
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
 edge_source.plot(show_edges=True)
 mesh = sg.delaunay_3d(edge_source)
