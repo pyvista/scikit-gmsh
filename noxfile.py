067eb28f5edb97cf9e56e906677dbde07976b7a8
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-09 16:32:23 +0900

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,14 +3,6 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
-def tests(session: nox.Session) -> None:
-    """Run the unit and regular tests."""
-    session.install(".[test]")
-    session.run("pip", "install", "-r", "requirements_test.txt", *session.posargs)
-    session.run("pytest", *session.posargs)
-
-
 import os
 import pathlib
 import shutil
@@ -18,6 +10,14 @@ import shutil
 import nox
 
 nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+def tests(session: nox.Session) -> None:
+    """Run the unit and regular tests."""
+    session.install(".[test]")
+    session.run("pip", "install", "-r", "requirements_test.txt", *session.posargs)
+    session.run("pytest", *session.posargs)
 
 ## Sphinx related options
 
