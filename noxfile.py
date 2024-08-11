3251c78c082d2100cfd5e98bdcd05062d1315bdb
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-11 18:15:41 +0900

    Update

--- noxfile.py
+++ noxfile.py
@@ -5,9 +5,8 @@ from __future__ import annotations
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.10", "3.11", "3.12"])
 def tests(session: nox.Session) -> None:
     """Run the unit and regular tests."""
     session.install(".[test]")
-    session.install("-r", "requirements_test.txt", *session.posargs)
     session.run("pytest", *session.posargs)
