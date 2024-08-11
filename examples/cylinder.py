1fa0e5391f90c2207729967a58d9a6783c54f38f
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-11 21:10:11 +0900

    Update

--- examples/cylinder.py
+++ examples/cylinder.py
@@ -8,6 +8,7 @@ import skgmsh as sg
 
 edge_source = pv.Cylinder(resolution=16)
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
-edge_source.plot(show_edges=True)
+edge_source.plot(show_edges=True, line_width=2, color="aliceblue", lighting=False, edge_color="gray")
 delaunay_3d = sg.Delaunay3D(edge_source)
-delaunay_3d.mesh.shrink(0.9).plot(show_edges=True)
+mesh = delaunay_3d.mesh
+mesh.shrink(0.9).plot(show_edges=True, line_width=1, color="aliceblue", lighting=False, edge_color="gray")
