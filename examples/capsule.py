d8150790154390625e22e927205ade0905a9cd46
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-11 21:06:34 +0900

    Update

--- examples/capsule.py
+++ examples/capsule.py
@@ -6,7 +6,7 @@ import pyvista as pv
 
 import skgmsh as sg
 
-edge_source = pv.Capsule(resolution=2)
+edge_source = pv.Capsule(resolution=3)
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
 edge_source.plot(show_edges=True)
 delaunay_3d = sg.Delaunay3D(edge_source)
