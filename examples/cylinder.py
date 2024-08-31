9c975bdd4170ccb1c1e788a8906b9e1712f35dac
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-31 14:35:58 +0900

    Update

--- examples/cylinder.py
+++ examples/cylinder.py
@@ -10,12 +10,14 @@ from __future__ import annotations
 
 import pyvista as pv
 
-# import skgmsh as sg  # noqa: ERA001
+import skgmsh as sg
 
 edge_source = pv.Cylinder(resolution=16)
+edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
 edge_source.plot(show_edges=True)
-# edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)  # noqa: ERA001
-# edge_source.plot(show_edges=True)  # noqa: ERA001
-# alg = sg.Delaunay3D(edge_source)  # noqa: ERA001
-# mesh = alg.mesh.shrink(0.9)  # noqa: ERA001
-# mesh.plot(show_edges=True)  # noqa: ERA001
+
+# %%
+# Generate the mesh.
+
+alg = sg.Delaunay3D(edge_source)
+alg.mesh.plot(show_edges=True)
--- examples/polygon_with_hole.py
+++ examples/polygon_with_hole.py
@@ -8,14 +8,14 @@ Polygon with hole geometry example.
 
 from __future__ import annotations
 
-# import skgmsh as sg  # noqa: ERA001
-import pyvista as pv
+import skgmsh as sg
 
-pv.Cube().plot(color="w", show_edges=True)
+shell = [(0, 0, 0), (0, 10, 0), (10, 10, 0), (10, 0, 0), (0, 0, 0)]
+holes = [[(2, 2, 0), (2, 4, 0), (4, 4, 0), (4, 2, 0), (2, 2, 0)]]
+alg = sg.Delaunay2D(shell=shell, holes=holes)
 
-# shell = [(0, 0, 0), (0, 10, 0), (10, 10, 0), (10, 0, 0), (0, 0, 0)]  # noqa: ERA001
-# holes = [[(2, 2, 0), (2, 4, 0), (4, 4, 0), (4, 2, 0), (2, 2, 0)]]  # noqa: ERA001
-#
-# alg = sg.Delaunay2D(shell=shell, holes=holes)  # noqa: ERA001
-# mesh = alg.mesh  # noqa: ERA001
-# mesh.plot(show_edges=True, color="white", cpos="xy")  # noqa: ERA001
+# %%
+# Generate the mesh.
+
+mesh = alg.mesh
+mesh.plot(show_edges=True, color="white", cpos="xy")
