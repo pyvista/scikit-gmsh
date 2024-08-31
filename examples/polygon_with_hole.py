3f13aba6357ee0e5473ac9f4bf28d644dac32f47
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-31 16:32:26 +0900

    Update polygon_with_hole.py

--- examples/polygon_with_hole.py
+++ examples/polygon_with_hole.py
@@ -13,6 +13,7 @@ import skgmsh as sg
 shell = [(0, 0, 0), (0, 10, 0), (10, 10, 0), (10, 0, 0), (0, 0, 0)]
 holes = [[(2, 2, 0), (2, 4, 0), (4, 4, 0), (4, 2, 0), (2, 2, 0)]]
 alg = sg.Delaunay2D(shell=shell, holes=holes)
+
 # %%
 # Generate the mesh.
 
