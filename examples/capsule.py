814cd4efbe9c7119c77f58d199f9b115af7979a1
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-31 19:42:24 +0900

    Update capsule.py

--- examples/capsule.py
+++ examples/capsule.py
@@ -9,5 +9,5 @@ import skgmsh as sg
 edge_source = pv.Capsule(resolution=3)
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
 edge_source.plot(show_edges=True)
-delaunay_3d = sg.Delaunay3D(edge_source)
-delaunay_3d.mesh.shrink(0.9).plot(show_edges=True)
+alg = sg.Delaunay3D(edge_source)
+alg.mesh.shrink(0.9).plot(show_edges=True)
