adc5ba1300d931a8b50888bc5292e004929b7fca
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-31 13:16:12 +0900

    Update polygon_with_hole.py

--- examples/polygon_with_hole.py
+++ examples/polygon_with_hole.py
@@ -14,4 +14,8 @@ shell = [(0, 0, 0), (0, 10, 0), (10, 10, 0), (10, 0, 0), (0, 0, 0)]
 holes = [[(2, 2, 0), (2, 4, 0), (4, 4, 0), (4, 2, 0), (2, 2, 0)]]
 
 alg = sg.Delaunay2D(shell=shell, holes=holes)
+
+# %%
+# Plot generated mesh.
+
 alg.mesh.plot(show_edges=True, color="white", cpos="xy")
