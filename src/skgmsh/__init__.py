463e527828f8c19c49c5fb04ed398ad59b0a1531
Tetsuo Koyama <tkoyama010@gmail.com>
2024-09-04 10:19:11 +0900

    Bump to 0.2.1

--- src/skgmsh/__init__.py
+++ src/skgmsh/__init__.py
@@ -28,7 +28,7 @@ FALSE = 0
 now = datetime.datetime.now(tz=datetime.timezone.utc)
 
 # major, minor, patch
-version_info = 0, 2, 0
+version_info = 0, 2, 1
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
