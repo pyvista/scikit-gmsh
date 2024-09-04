d65fb5d87034882f6b706e64831752aced1015d9
Tetsuo Koyama <tkoyama010@gmail.com>
2024-09-04 17:28:04 +0900

    Update capsule.py

--- examples/capsule.py
+++ examples/capsule.py
@@ -6,7 +6,7 @@ Capsule geometry example.
 
 """
 
-# sphinx_gallery_thumbnail_number = 1 # noqa:ERA001
+# sphinx_gallery_thumbnail_number = 3 # noqa:ERA001
 
 from __future__ import annotations
 
@@ -16,16 +16,16 @@ import skgmsh as sg
 
 edge_source = pv.Capsule(resolution=10)
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
-edge_source.plot(show_edges=True)
+edge_source.plot(show_edges=True, color="white")
 
 # %%
 # Create a 3D mesh from the edge source.
 
 alg = sg.Delaunay3D(edge_source)
-alg.mesh.shrink(0.9).plot(show_edges=True)
+alg.mesh.shrink(0.9).plot(show_edges=True, color="white")
 
 # %%
 # Change the cell size of the mesh.
 
-alg.cell_size = 0.5
-alg.mesh.plot(show_edges=True)
+alg.cell_size = 0.25
+alg.mesh.plot(show_edges=True, color="white")
