daba3212c7a93b9bda7833a7a9501f6f98f7560d
Tetsuo Koyama <tkoyama010@gmail.com>
2024-09-04 10:06:18 +0900

    Update

--- examples/capsule.py
+++ examples/capsule.py
@@ -14,7 +14,7 @@ import pyvista as pv
 
 import skgmsh as sg
 
-edge_source = pv.Capsule(resolution=9)
+edge_source = pv.Capsule(resolution=10)
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
 edge_source.plot(show_edges=True)
 
