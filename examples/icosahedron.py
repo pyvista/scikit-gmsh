7690d216e3ee536a0772f5998162b90bf6ba5b1f
Tetsuo Koyama <tkoyama010@gmail.com>
2024-09-06 14:48:44 +0900

    Update icosahedron.py

--- examples/icosahedron.py
+++ examples/icosahedron.py
@@ -16,10 +16,10 @@ import skgmsh as sg
 
 edge_source = pv.Icosahedron()
 edge_source.merge(pv.PolyData(edge_source.points), merge_points=True, inplace=True)
-edge_source.plot(show_edges=True)
+edge_source.plot(show_edges=True, color="white")
 
 # %%
 # Generate the mesh.
 
 delaunay_3d = sg.Delaunay3D(edge_source)
-delaunay_3d.mesh.shrink(0.9).plot(show_edges=True)
+delaunay_3d.mesh.shrink(0.9).plot(show_edges=True, color="white")
