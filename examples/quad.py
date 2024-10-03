6949951a369cd2e447f04235c1b61be0e6d5eee4
Tetsuo Koyama <tkoyama010@gmail.com>
2024-10-03 17:42:09 +0900

    Update quad.py

--- examples/quad.py
+++ examples/quad.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 import gmsh
+import pyvista as pv
 
 gmsh.initialize()
 gmsh.open("quad.geo")
 gmsh.model.mesh.generate(2)
 gmsh.write("quad.vtk")
 gmsh.finalize()
-
-import pyvista as pv
 
 mesh = pv.read("quad.vtk")
 mesh.plot(show_edges=True, color="w", cpos="xy")
