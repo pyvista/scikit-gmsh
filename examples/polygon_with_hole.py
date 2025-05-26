d8c88c9f86918a0c956ad7e652ed0b6ca5f1bba5
Tetsuo Koyama <tkoyama010@gmail.com>
2025-05-26 22:46:59 +0900

    Update polygon_with_hole.py

--- examples/polygon_with_hole.py
+++ examples/polygon_with_hole.py
@@ -29,9 +29,6 @@ alg.mesh.plot(show_edges=True, cpos="xy", color="white", line_width=2)
 
 # %%
 
-shell = [(0, 0, 0), (0, 10, 0), (10, 10, 0), (10, 0, 0), (0, 0, 0)]
-holes = [[(2, 2, 0), (2, 4, 0), (4, 4, 0), (4, 2, 0), (2, 2, 0)]]
-alg = sg.Delaunay2D(shell=shell, holes=holes)
 alg.enable_recombine()
 
 # %%
